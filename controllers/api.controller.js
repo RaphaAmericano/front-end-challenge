43297be3d7c545bc3e5940573770dc95a625d6ef
RaphaAmericano <raphael@raphaelamericano.com.br>
2018-06-16 00:28:56 -0300

    Adicionando dados de top 5 artistas

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,5 +1,17 @@
-angular.module("mainApp", [])
-    .controller("mainController", function($scope, $http){
+angular.module("mainApp", []).controller("mainController", function($scope, $http){
         $scope.date = new Date();
-        console.log($scope.date);
+        $scope.topbandas = []
+        $scope.dados = [];
+
+        var apiKey = '&api_key=b80de9cb1cfe2782055d664a50ab903a&format=json';
+        var apiUrl = 'http://ws.audioscrobbler.com/2.0/';
+
+        var carregarApi = function(parametro){
+            $http.get(apiUrl+parametro+apiKey).then(function(data, status){
+                
+                $scope.topbandas = data;
+                console.log($scope.topbandas);
+            });
+        };
+        carregarApi("?method=chart.gettopartists&page=1&limit=5");
 });
