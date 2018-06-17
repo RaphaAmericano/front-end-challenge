a252763b604997009f1e0008ba6e0ba1435eb067
RaphaAmericano <raphael@raphaelamericano.com.br>
2018-06-17 02:16:24 -0300

    Adicao do gulp, correcoes do ng-class e estilizacao com scss

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,17 +1,33 @@
 angular.module("mainApp", []).controller("mainController", function($scope, $http){
         $scope.date = new Date();
-        $scope.topbandas = []
+        $scope.topbandas = [];
+        $scope.topmusicas = [];
         $scope.dados = [];
+
+        
 
         var apiKey = '&api_key=b80de9cb1cfe2782055d664a50ab903a&format=json';
         var apiUrl = 'http://ws.audioscrobbler.com/2.0/';
 
-        var carregarApi = function(parametro){
-            $http.get(apiUrl+parametro+apiKey).then(function(data, status){
-                
-                $scope.topbandas = data;
+        var carregarApi = function(){
+            $http.get(apiUrl+"?method=chart.gettopartists&page=1&limit=5"+apiKey).then(function(data, status){
+                $scope.topbandas = data.data.artists.artist;
                 console.log($scope.topbandas);
             });
+            $http.get(apiUrl+"?method=chart.gettoptracks&page=1&limit=3"+apiKey).then(function(data, status){
+                $scope.topmusicas = data.data.tracks.track;
+                console.log($scope.topmusicas);
+            });
         };
-        carregarApi("?method=chart.gettopartists&page=1&limit=5");
+
+        var atualizarTopMusicas = function($scope, pagina){
+            $scope.topmusicas = [];
+            $http.get(apiUrl+"?method=chart.gettoptracks&page="+pagina+"&limit=3"+apiKey).then(function(data, status){
+                $scope.topmusicas = data.data.tracks.track;
+                console.log($scope.topmusicas);
+            });
+        };
+
+        carregarApi();
+
 });
---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('sass', function () {
+  //return gulp.src('./sass/**/*.scss')
+  return gulp.src('./sass/style.scss')
+    .pipe(sass({
+      includePaths: [
+        'node_modules',
+        '/node_modules/bootstrap-sass/assets/stylesheets']
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+});
+
+gulp.task('bootstrap-sass', function() {
+  //return gulp.src('./css/app.scss')
+  return gulp.src('node_modules/bootstrap-sass/assets/stylesheets/**/*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('./css'));
+});
+
+gulp.task('browserSync', function(){
+  browserSync.init({
+    server: {
+      baseDir: './',
+      index: 'app.html',
+      directory: false
+    }
+  })
+});
+
+gulp.task('sass:watch',['browserSync', 'sass'], function () {
+  gulp.watch('./sass/*.scss', ['sass']);
+  gulp.watch('app.html', browserSync.reload);
+  gulp.watch('./sass/*.scss', browserSync.reload);
+
+});
