b58d3718cc598e43cedaad0354ea09750e82a05e
RaphaAmericano <raphael@raphaelamericano.com.br>
2018-06-21 00:37:37 -0300

    Ajuste do display da busca reorganizacao e inicio do roteamento

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,15 +1,18 @@
-angular.module("mainApp", []).service('buscaArtista', ['$http', function($http){      
-    return {
-        search: function(keywords){
-            return $http.get('http://ws.audioscrobbler.com/2.0/?method=artist.search&artist='+keywords+'&api_key=b80de9cb1cfe2782055d664a50ab903a&format=json')
-            .then(function(data, status){
-                //mudar para listar os artistas
-                //$scope.topbandas = data.data.artists.artist;
-                console.log(data);
-            });
-        }
-    }
-}]).controller("mainController", function($scope, $http){
+var app = angular.module("mainApp", []);
+
+// app.service('buscaArtista', ['$http', function($http){      
+//     return {
+//         search: function(keywords){
+//             return $http.get('http://ws.audioscrobbler.com/2.0/?method=artist.search&artist='+keywords+'&api_key=b80de9cb1cfe2782055d664a50ab903a&format=json')
+//             .then(function(data, status){
+//                 //mudar para listar os artistas
+//                 //$scope.topbandas = data.data.artists.artist;
+//                 console.log(data);
+//             });
+//         }
+//     }
+// }]);
+app.controller("mainController", function($scope, $http){
         $scope.date = new Date();
         $scope.topbandas = [];
         $scope.topmusicas = [];
@@ -45,7 +48,24 @@ angular.module("mainApp", []).service('buscaArtista', ['$http', function($http){
                 console.log(data.data.results.artistmatches.artist);
             });
         };
+        
+        $scope.limparBusca = function(){
+            $scope.resultado = [];
+        }
+
 
         carregarApi();
 
 });
+
+// app.config(function($routeProvider){
+//     $routeProvider
+//     // .when('/', {
+//     //     templateUrl: 'app.html',
+//     //     controller: 'mainControlle'
+//     // })
+//     .when('/artista', {
+//         templateUrl: 'views/artista.html',
+//         controller: 'mainController'
+//     })
+// });
