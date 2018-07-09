cf0df3e3b42bf27e62fb0be277f848ca308b74ac
Raphael Americano <raphael.americano@avantare.intranet>
2018-07-09 11:09:57 -0300

    Get dos dados na view do artista

--- artista/artista.controller.js
+++ artista/artista.controller.js
@@ -1,6 +1,22 @@
-angular.module('mainApp').controller('artistaController', artistaController);
+angular.module('mainApp').controller('artistaController',['$routeParams', 'callApi', artistaController]);
 
-function artistaController(){
+function artistaController($routeParams, callApi){
     var vm = this;
-    vm.teste = ['hello world'];
+    vm.params = $routeParams;
+    vm.artistaUrl = $routeParams['name'];
+    var stringArtista = vm.artistaUrl;
+    stringArtista = stringArtista.replace(' ', '+');
+    vm.parametro = 'artist.getinfo&artist='+stringArtista;
+    vm.service = callApi;
+    vm.artista = [];
+    
+    vm.dados = function(){
+        callApi.getApi(vm.parametro).then(function(response){
+            vm.artista = response.data;
+            console.log(vm.dados);
+        }, function(err){
+            console.log(err)
+        })
+    }
+    vm.dados();
 }
--- modules/app.module.js
+++ modules/app.module.js
@@ -1,10 +1,11 @@
-angular.module("mainApp").config(['$routeProvider', function($routeProvider){
+angular.module("mainApp").config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider){
     $routeProvider
     .when('/', {
         templateUrl: '../views/main.html'
     })
-    .when('/artista/', {
+    .when('/artista/:name', {
         templateUrl: '../views/artista_view.html'
     })
     .otherwise({redirectTo: '/'});
+    $locationProvider.html5Mode(true);
 }]);
